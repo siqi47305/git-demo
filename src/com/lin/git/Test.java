4d23d31bd15db869bd8e56a54e238dcf13bd71d3
linshiqi <l_fortyseven@163.com>
2022-08-09 17:21:59 +0800

    hot_fix_commit02

--- src/com/lin/git/Test.java
+++ src/com/lin/git/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("testjson01");
         System.out.println("testjson02");
-        System.out.println("testjson03");
+        System.out.println("testjson05");
     }
 }
