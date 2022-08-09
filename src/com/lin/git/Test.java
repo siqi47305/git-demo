e74aba265832975d3be4d69915e0988506a5fdf0
linshiqi <l_fortyseven@163.com>
2022-08-09 20:00:19 +0800

    push test

--- src/com/lin/git/Test.java
+++ src/com/lin/git/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("master_test01");
         System.out.println("hot_fix_test01");
+        System.out.println("push test");
     }
 }
