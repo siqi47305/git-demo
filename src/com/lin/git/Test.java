7814b09b2f15492444d00504c062efe2b5e1f574
linshiqi <l_fortyseven@163.com>
2022-08-09 17:13:41 +0800

    third_commit

--- src/com/lin/git/Test.java
+++ src/com/lin/git/Test.java
@@ -3,5 +3,7 @@ package com.lin.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("testjson01");
+        System.out.println("testjson02");
+        System.out.println("testjson03");
     }
 }
